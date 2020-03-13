1604b00f5955e4f783aa9724b40dc76051e679b8
Petr Dlouhý <petr.dlouhy@email.cz>
2020-03-13 20:02:38 +0100

    fix ValueError in tests

--- categories/fields.py
+++ categories/fields.py
@@ -3,18 +3,16 @@ from django.db.models import ForeignKey, ManyToManyField
 
 class CategoryM2MField(ManyToManyField):
     def __init__(self, **kwargs):
-        from .models import Category
         if 'to' in kwargs:
             kwargs.pop('to')
-        super(CategoryM2MField, self).__init__(to=Category, **kwargs)
+        super(CategoryM2MField, self).__init__(to='categories.Category', **kwargs)
 
 
 class CategoryFKField(ForeignKey):
     def __init__(self, **kwargs):
-        from .models import Category
         if 'to' in kwargs:
             kwargs.pop('to')
-        super(CategoryFKField, self).__init__(to=Category, **kwargs)
+        super(CategoryFKField, self).__init__(to='categories.Category', **kwargs)
 
 
 try:
